ddbbd1eeb86a2e85f892c2274a8c5a5cd229625a
andremd <andremacdowell@gmail.com>
2016-05-02 14:56:44 -0300

    navegação entre as telas concluida

---
+++ Assets/View/Scripts/GameView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace View
+{
+	public class GameView : MonoBehaviour 
+	{
+
+		public void OnQuitClicked()
+		{
+			SceneManager.LoadScene (0);
+		}
+
+	}
+}
