40f4207fa8d6a7b1cb32d00f6794780cf4ac4fe9
andremd <andremacdowell@gmail.com>
2016-05-02 14:26:28 -0300

    menu carregando a cena de jogo

---
+++ Assets/View/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace View
+{
+	public class SceneLoader : MonoBehaviour 
+	{
+		public void OnPossibleClick()
+		{
+			SceneManager.LoadScene (1);
+		}
+
+		public void OnImpossibleClick()
+		{
+			SceneManager.LoadScene (1);
+		}
+
+		public void OnQuitClick()
+		{
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+				Application.Quit ();
+			#endif
+		}
+
+	}
+}
